6aed98edc56cb78aa9f3acf8c143cab01f9f811d
Saifultazali <56445770+Saifultazali@users.noreply.github.com>
2019-10-26 15:20:17 +0700

    Add files via upload
    Tugas 1

---
+++ tugas 01(saiful).py
@@ -0,0 +1,11 @@
+#INI Variabel & Nilai
+
+namalengkap = "Saiful Tazali"
+namapanggilan = "Saiful"
+npm = "311910625"
+tempattgllahir = "Kebumen"
+umur = "20"
+nohp = "+6281384811098"
+alamat = "Desa Patemon, Kecamatan Gombong, Kabupaten Kebumen"
+
+print("Assalamu'alaikum Wr. Wb, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,".\n My NPM is",npm,".\n I was born in",tempattgllahir,",and i am",umur,"years old.\n I am very glad if you want to invite my house in",alamat,".\n So, don't forget to call me before with number",nohp,"\n Thank You")
